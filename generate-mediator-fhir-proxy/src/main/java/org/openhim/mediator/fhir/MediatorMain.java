804e9590000c3ea54e2eac4684043b471e09f117
Pranav9357 <pranav.vichare@tntra.io>
2022-07-19 18:54:02 +0530

    Successfully communicating with opencrvs & fetch the data via fhir-medator. Right now using hardcore data but configuring & implementing dynamic data

--- generate-mediator-fhir-proxy/src/main/java/org/openhim/mediator/fhir/MediatorMain.java
+++ generate-mediator-fhir-proxy/src/main/java/org/openhim/mediator/fhir/MediatorMain.java
@@ -12,10 +12,13 @@ import akka.event.LoggingAdapter;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.openhim.mediator.engine.*;
+import scala.util.parsing.combinator.testing.Str;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Properties;
 
@@ -88,6 +91,41 @@ public class MediatorMain {
         return config;
     }
 
+    public static void postRequest() throws IOException {
+        URL url = new URL("http://localhost:7070/graphql");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJyZWdpc3RlciIsInBlcmZvcm1hbmNlIiwiY2VydGlmeSIsImRlbW8iXSwiaWF0IjoxNjU4MjMzNDQ3LCJleHAiOjE2NTg4MzgyNDcsImF1ZCI6WyJvcGVuY3J2czphdXRoLXVzZXIiLCJvcGVuY3J2czp1c2VyLW1nbnQtdXNlciIsIm9wZW5jcnZzOmhlYXJ0aC11c2VyIiwib3BlbmNydnM6Z2F0ZXdheS11c2VyIiwib3BlbmNydnM6bm90aWZpY2F0aW9uLXVzZXIiLCJvcGVuY3J2czp3b3JrZmxvdy11c2VyIiwib3BlbmNydnM6c2VhcmNoLXVzZXIiLCJvcGVuY3J2czptZXRyaWNzLXVzZXIiLCJvcGVuY3J2czpjb3VudHJ5Y29uZmlnLXVzZXIiLCJvcGVuY3J2czp3ZWJob29rcy11c2VyIiwib3BlbmNydnM6Y29uZmlnLXVzZXIiXSwiaXNzIjoib3BlbmNydnM6YXV0aC1zZXJ2aWNlIiwic3ViIjoiNjJiOTljZDk4ZjExM2E3MDBjYzE5YTFhIn0.aDY4VoePBjkgg-RgUEpnhbENV2MVh2PrIqrfRDepI1aPjrdBsCHAer3fmH2P1nKl7gJhSYB_vbPNAljF7lpOMQh2O6cP1djXZBXLp0VEtICdVxxqcGzJ9Cds3ev8UtvUhEhYpG7DBcSX_J4gHgj8W4WLeW-W0ovD51-dzsfzTECWjw9tO60gFfI22kje0-JQB48pi2QZB1gSaK6qOn-MJHlaVlTSuMj83endojYrpkWsWlBn4Ov8os_C89JUZa1zc-9D0ll6mk-VEJYAyS-z96DbYbmsRE9xHKYODnDlmM_HVR9pL_zPSJlZ5O0n4o4jTddv-SAxLnEtg9HFB_9rwg");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        String data = "{\"operationName\":\"searchEvents\",\"variables\":{\"locationIds\":null,\"sort\":\"DESC\",\"trackingId\":\"\",\"registrationNumber\":\"\",\"contactNumber\":\"\",\"name\":\"patel\"},\"query\":\"query searchEvents($sort: String, $trackingId: String, $contactNumber: String, $registrationNumber: String, $name: String, $locationIds: [String!]) {\\n  searchEvents(\\n    sort: $sort\\n    trackingId: $trackingId\\n    registrationNumber: $registrationNumber\\n    name: $name\\n    contactNumber: $contactNumber\\n    locationIds: $locationIds\\n  ) {\\n    totalItems\\n    results {\\n      id\\n      type\\n      registration {\\n        status\\n        contactNumber\\n        trackingId\\n        registrationNumber\\n        registeredLocationId\\n        duplicates\\n        assignment {\\n          userId\\n          firstName\\n          lastName\\n          officeName\\n          __typename\\n        }\\n        createdAt\\n        modifiedAt\\n        __typename\\n      }\\n      ... on BirthEventSearchSet {\\n        dateOfBirth\\n        childName {\\n          firstNames\\n          familyName\\n          use\\n          __typename\\n        }\\n        __typename\\n      }\\n      ... on DeathEventSearchSet {\\n        dateOfDeath\\n        deceasedName {\\n          firstNames\\n          familyName\\n          use\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}";
+
+        byte[] out = data.getBytes(StandardCharsets.UTF_8);
+
+        OutputStream stream = connection.getOutputStream();
+        stream.write(out);
+
+        System.out.println("Response code" + connection.getResponseCode());
+
+        StringBuilder sb = new StringBuilder();
+        int HttpResult = connection.getResponseCode();
+        if (HttpResult == HttpURLConnection.HTTP_OK) {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            br.close();
+            System.out.println("" + sb);
+        } else {
+            System.out.println("Response message" + connection.getResponseMessage());
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         //setup actor system
         final ActorSystem system = ActorSystem.create("mediator");
@@ -122,6 +160,9 @@ public class MediatorMain {
         server.start();
 
         log.info(String.format("%s listening on %s:%s", config.getName(), config.getServerHost(), config.getServerPort()));
+
+        postRequest();
+
         Thread.currentThread().join();
     }
 }
