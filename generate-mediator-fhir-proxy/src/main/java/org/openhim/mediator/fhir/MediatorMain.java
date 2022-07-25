20c466808f41c0b27c4b8dbb1c3b738a6de739d8
Pranav9357 <pranav.vichare@tntra.io>
2022-07-25 11:39:14 +0530

    Succesfully communicate with opencrvs with response code & message

--- generate-mediator-fhir-proxy/src/main/java/org/openhim/mediator/fhir/MediatorMain.java
+++ generate-mediator-fhir-proxy/src/main/java/org/openhim/mediator/fhir/MediatorMain.java
@@ -56,6 +56,7 @@ public class MediatorMain {
         config.setServerPort( Integer.parseInt(config.getProperty("mediator.port")) );
         config.setRootTimeout(Integer.parseInt(config.getProperty("mediator.timeout")));
 
+
         config.setCoreHost(config.getProperty("core.host"));
         config.setCoreAPIUsername(config.getProperty("core.api.user"));
         config.setCoreAPIPassword(config.getProperty("core.api.password"));
@@ -92,16 +93,17 @@ public class MediatorMain {
     }
 
     public static void postRequest() throws IOException {
-        URL url = new URL("http://localhost:7070/graphql");
+
+        URL url = new URL("http://localhost:5001/fhir");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setDoInput(true);
         connection.setDoOutput(true);
         connection.setRequestMethod("POST");
-        connection.setRequestProperty("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJyZWdpc3RlciIsInBlcmZvcm1hbmNlIiwiY2VydGlmeSIsImRlbW8iXSwiaWF0IjoxNjU4MjMzNDQ3LCJleHAiOjE2NTg4MzgyNDcsImF1ZCI6WyJvcGVuY3J2czphdXRoLXVzZXIiLCJvcGVuY3J2czp1c2VyLW1nbnQtdXNlciIsIm9wZW5jcnZzOmhlYXJ0aC11c2VyIiwib3BlbmNydnM6Z2F0ZXdheS11c2VyIiwib3BlbmNydnM6bm90aWZpY2F0aW9uLXVzZXIiLCJvcGVuY3J2czp3b3JrZmxvdy11c2VyIiwib3BlbmNydnM6c2VhcmNoLXVzZXIiLCJvcGVuY3J2czptZXRyaWNzLXVzZXIiLCJvcGVuY3J2czpjb3VudHJ5Y29uZmlnLXVzZXIiLCJvcGVuY3J2czp3ZWJob29rcy11c2VyIiwib3BlbmNydnM6Y29uZmlnLXVzZXIiXSwiaXNzIjoib3BlbmNydnM6YXV0aC1zZXJ2aWNlIiwic3ViIjoiNjJiOTljZDk4ZjExM2E3MDBjYzE5YTFhIn0.aDY4VoePBjkgg-RgUEpnhbENV2MVh2PrIqrfRDepI1aPjrdBsCHAer3fmH2P1nKl7gJhSYB_vbPNAljF7lpOMQh2O6cP1djXZBXLp0VEtICdVxxqcGzJ9Cds3ev8UtvUhEhYpG7DBcSX_J4gHgj8W4WLeW-W0ovD51-dzsfzTECWjw9tO60gFfI22kje0-JQB48pi2QZB1gSaK6qOn-MJHlaVlTSuMj83endojYrpkWsWlBn4Ov8os_C89JUZa1zc-9D0ll6mk-VEJYAyS-z96DbYbmsRE9xHKYODnDlmM_HVR9pL_zPSJlZ5O0n4o4jTddv-SAxLnEtg9HFB_9rwg");
+        connection.setRequestProperty("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJyZWdpc3RlciIsInBlcmZvcm1hbmNlIiwiY2VydGlmeSIsImRlbW8iXSwiaWF0IjoxNjU4NzI1MjQwLCJleHAiOjE2NTkzMzAwNDAsImF1ZCI6WyJvcGVuY3J2czphdXRoLXVzZXIiLCJvcGVuY3J2czp1c2VyLW1nbnQtdXNlciIsIm9wZW5jcnZzOmhlYXJ0aC11c2VyIiwib3BlbmNydnM6Z2F0ZXdheS11c2VyIiwib3BlbmNydnM6bm90aWZpY2F0aW9uLXVzZXIiLCJvcGVuY3J2czp3b3JrZmxvdy11c2VyIiwib3BlbmNydnM6c2VhcmNoLXVzZXIiLCJvcGVuY3J2czptZXRyaWNzLXVzZXIiLCJvcGVuY3J2czpjb3VudHJ5Y29uZmlnLXVzZXIiLCJvcGVuY3J2czp3ZWJob29rcy11c2VyIiwib3BlbmNydnM6Y29uZmlnLXVzZXIiXSwiaXNzIjoib3BlbmNydnM6YXV0aC1zZXJ2aWNlIiwic3ViIjoiNjJiOTljZDk4ZjExM2E3MDBjYzE5YTFhIn0.uTMpyTLhyt7ucyhkIG-dqEBNvSZQe04AcxNVlva5DJSN5JGyUBQzeUQcMVZmbpQf-c-Y4QBtASChCuILh_UI0YggNOMdooTJOtJdzzY4nv-PxFL8UNa5hrUjYUPBeblGY8_3NBsBSC4oHaHF0z8I78lpYrl_ILJtQnq51bw6Ji14kcbvbpx7M2ugmTD4-H-yAiRcMBPaZvd3AVsAuELiTtvDhowNI2XWawGzd1xBuwRFqsWa3234MBLp31dGhpqrSF4W6Ir2lb7Mw4gOcqstXliywbyjlqdJfUD0RNa79h-IEamwEkGkNzPXcsj9JL11p1mE2tECY1YdnouHMspQvw");
         connection.setRequestProperty("Accept", "application/json");
         connection.setRequestProperty("Content-Type", "application/json");
 
-        String data = "{\"operationName\":\"searchEvents\",\"variables\":{\"locationIds\":null,\"sort\":\"DESC\",\"trackingId\":\"\",\"registrationNumber\":\"\",\"contactNumber\":\"\",\"name\":\"patel\"},\"query\":\"query searchEvents($sort: String, $trackingId: String, $contactNumber: String, $registrationNumber: String, $name: String, $locationIds: [String!]) {\\n  searchEvents(\\n    sort: $sort\\n    trackingId: $trackingId\\n    registrationNumber: $registrationNumber\\n    name: $name\\n    contactNumber: $contactNumber\\n    locationIds: $locationIds\\n  ) {\\n    totalItems\\n    results {\\n      id\\n      type\\n      registration {\\n        status\\n        contactNumber\\n        trackingId\\n        registrationNumber\\n        registeredLocationId\\n        duplicates\\n        assignment {\\n          userId\\n          firstName\\n          lastName\\n          officeName\\n          __typename\\n        }\\n        createdAt\\n        modifiedAt\\n        __typename\\n      }\\n      ... on BirthEventSearchSet {\\n        dateOfBirth\\n        childName {\\n          firstNames\\n          familyName\\n          use\\n          __typename\\n        }\\n        __typename\\n      }\\n      ... on DeathEventSearchSet {\\n        dateOfDeath\\n        deceasedName {\\n          firstNames\\n          familyName\\n          use\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}";
+        String data = "{\"resourceType\":\"Bundle\",\"type\":\"document\",\"entry\":[{\"fullUrl\":\"http://localhost:3447/fhir/Task/18cff33f-1a05-4bee-82e2-cb2eb874ef06/_history/8a1db5bb-7e23-4967-a26f-4c5449fcedf7\",\"resource\":{\"resourceType\":\"Task\",\"status\":\"requested\",\"code\":{\"coding\":[{\"system\":\"http://opencrvs.org/specs/types\",\"code\":\"BIRTH\"}]},\"focus\":{\"reference\":\"Composition/619ee636-a5bc-4f35-b7f3-b0dc887f4a1e\"},\"id\":\"18cff33f-1a05-4bee-82e2-cb2eb874ef06\",\"identifier\":[{\"system\":\"http://opencrvs.org/specs/id/draft-id\",\"value\":\"619ee636-a5bc-4f35-b7f3-b0dc887f4a1e\"},{\"system\":\"http://opencrvs.org/specs/id/birth-tracking-id\",\"value\":\"BORSWWY\"},{\"system\":\"http://opencrvs.org/specs/id/birth-registration-number\",\"value\":\"2022BORSWWY\"}],\"extension\":[{\"url\":\"http://opencrvs.org/specs/extension/contact-person\",\"valueString\":\"MOTHER\"},{\"url\":\"http://opencrvs.org/specs/extension/contact-person-phone-number\",\"valueString\":\"+260754684646\"},{\"url\":\"http://opencrvs.org/specs/extension/timeLoggedMS\",\"valueInteger\":13632},{\"url\":\"http://opencrvs.org/specs/extension/regLastLocation\",\"valueReference\":{\"reference\":\"Location/b09122df-81f8-41a0-b5c6-68cba4145cab\"}},{\"url\":\"http://opencrvs.org/specs/extension/regLastOffice\",\"valueString\":\"Ibombo District Office\",\"valueReference\":{\"reference\":\"Location/c9c4d6e9-981c-4646-98fe-4014fddebd5e\"}},{\"url\":\"http://opencrvs.org/specs/extension/regLastUser\",\"valueReference\":{\"reference\":\"Practitioner/be8462b5-36a2-4f21-8d34-b4b679f7d3b3\"}},{\"url\":\"http://opencrvs.org/specs/extension/configuration\",\"valueReference\":{\"reference\":\"IN_CONFIGURATION\"}},{\"url\":\"http://opencrvs.org/specs/extension/regAssigned\",\"valueString\":\"REGISTERED\"},{\"url\":\"http://opencrvs.org/specs/extension/regDownloaded\",\"valueString\":\"REGISTERED\"}],\"lastModified\":\"2022-07-07T16:15:08.127Z\",\"businessStatus\":{\"coding\":[{\"system\":\"http://opencrvs.org/specs/reg-status\",\"code\":\"REGISTERED\"}]},\"meta\":{\"lastUpdated\":\"2022-07-07T16:15:08.633+00:00\",\"versionId\":\"8a1db5bb-7e23-4967-a26f-4c5449fcedf7\"}},\"request\":{\"method\":\"PUT\",\"url\":\"Task/18cff33f-1a05-4bee-82e2-cb2eb874ef06\"}}],\"signature\":{\"type\":[{\"code\":\"downloaded\"}],\"when\":\"Thu Jul 07 2022 21:45:36 GMT+0530 (India Standard Time)\"}}";
 
         byte[] out = data.getBytes(StandardCharsets.UTF_8);
 
@@ -127,6 +129,7 @@ public class MediatorMain {
     }
 
     public static void main(String[] args) throws Exception {
+
         //setup actor system
         final ActorSystem system = ActorSystem.create("mediator");
         //setup logger for main
